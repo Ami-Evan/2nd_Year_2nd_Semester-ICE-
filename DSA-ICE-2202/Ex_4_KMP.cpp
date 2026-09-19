cbf9c958b09b57f73d4a904364cf80613fc76eef
Evan <evansarker110@gmail.com>
2026-09-19 07:47:02 +0600

    some add

--- DSA-ICE-2202/Ex_4_KMP.cpp
+++ DSA-ICE-2202/Ex_4_KMP.cpp
@@ -1,15 +1,93 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
+
+// Create LPS array
+void createLPS(string pattern, int lps[])
+{
+    int m = pattern.length();
+
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+
+    while (i < m)
+    {
+        if (pattern[i] == pattern[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+// KMP Search
+void KMP(string text, string pattern)
+{
+    int n = text.length();
+    int m = pattern.length();
+
+    int lps[m];
+
+    createLPS(pattern, lps);
+
+    int i = 0;
+    int j = 0;
+
+    bool found = false;
+
+    while (i < n)
+    {
+        if (text[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+
+        if (j == m)
+        {
+            cout << "Pattern found at position: " << i - j << endl;
+
+            found = true;
+
+            j = lps[j - 1];
+        }
+        else if (i < n && text[i] != pattern[j])
+        {
+            if (j != 0)
+            {
+                j = lps[j - 1];
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Pattern not found!" << endl;
+    }
+}
 
 int main()
 {
-    string text, pattern;
-
-    // Text file open
     ifstream file("text.txt");
 
     if (!file)
@@ -18,44 +96,41 @@ int main()
         return 0;
     }
 
-    // File theke text read
-    getline(file, text);
+    string text;
+    string line;
+
+    // Read complete text file
+    while (getline(file, line))
+    {
+        text += line;
+        text += '\n';
+    }
 
     file.close();
 
-    // Pattern input
-    cout << "Enter the pattern: ";
-    getline(cin, pattern);
+    string pattern;
 
-    int n = text.length();
-    int m = pattern.length();
+    cout << "Enter word to search: ";
+    cin >> pattern;
 
-    bool found = false;
-
-    // Pattern Matching
-    for (int i = 0; i <= n - m; i++)
-    {
-        int j;
-
-        for (j = 0; j < m; j++)
-        {
-            if (text[i + j] != pattern[j])
-            {
-                break;
-            }
-        }
-
-        if (j == m)
-        {
-            cout << "Pattern found at position: " << i << endl;
-            found = true;
-        }
-    }
-
-    if (!found)
-    {
-        cout << "Pattern not found!" << endl;
-    }
+    KMP(text, pattern);
 
     return 0;
 }
+
+
+/*
+text.txt এ যেমন লিখতে পারো
+Hello, my name is Evan.
+I am learning C++ and Competitive Programming.
+I love programming and I practice programming every day.
+
+Program run করলে:
+
+Enter word to search: programming
+
+Output:
+
+Pattern found at position: 57
+Pattern found at position: 79
+*/
